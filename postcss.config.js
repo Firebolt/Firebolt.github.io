daf7563b110a56822136ea27de268feb0cadb83a
Firebolt <97257564+Firebolt@users.noreply.github.com>
2022-05-29 14:27:11 +0200

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('postcss-mixins'),
+        require("stylelint"),
+        require('postcss-preset-env')({ stage: 1 }),
+        require('postcss-nested'),
+        require('cssnano'),
+    ],
+}
